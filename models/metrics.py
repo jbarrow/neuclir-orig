d0a9ff437edd876b72978b35e8b4f5228851416a
Joe Barrow <joseph.d.barrow@gmail.com>
2018-12-18 11:58:59 -0500

    Fleshing out metrics

--- models/metrics.py
+++ models/metrics.py
@@ -1,43 +1,55 @@
+import torch
+
 from typing import Optional
 from overrides import overrides
-
 from allennlp.training.metrics.metric import Metric
 
-class InformationRetrievalMetric(Metric):
-    def __init__(self):
-        super(InformationRetrievalMetric, self).__init__()
-
-    def __call__(self, output, targets):
-        return 1.0
-
-    def get_metric(self, reset: bool = False):
-        # compute the (QxD matrix)
-        return self._get_metric()
-
-    def _get_metric(self):
-        raise NotImplementedError(
-            'Subclass of InformationRetrievalMetric must implement its own _get_metric')
-
-    def reset(self):
-        print('*** RESET CALLED')
 
 @Metric.register('map')
-class MeanAveragePrecision(InformationRetrievalMetric):
-    def _get_metric(self):
+class MeanAveragePrecision(Metric):
+    def __init__(self, k: int = 1000):
+        super(MeanAveragePrecision, self).__init__()
+
+        self.k = k
+
+    def __call__(self, outputs: torch.Tensor, targets: torch.LongTensor):
+        pass
+
+    def get_metric(self, reset: bool = False):
         return 0.0
 
+    def reset(self):
+        pass
+
+
+@Metric.register('aqwv')
+class AQWV(Metric):
+    def __init__(self, beta: float = 40., cutoff: int = 40):
+        super(AQWV, self).__init__()
+
+        self.beta = beta
+        self.cutoff = cutoff
+
+    def __call__(self, outputs: torch.Tensor, targets: torch.LongTensor):
+        pass
+
+    def get_metric(self, reset: bool = False):
+        return 0.0
+
+    def reset(self):
+        pass
+
+
 @Metric.register('mrr')
-class MeanReciprocalRank(InformationRetrievalMetric):
+class MeanReciprocalRank(Metric):
     def __init__():
         pass
 
-    def _get_metric(self):
+    def __call__(self, outputs: torch.Tensor, targets: torch.LongTensor):
         pass
 
-@Metric.register('aqwv')
-class AQWV(InformationRetrievalMetric):
-    def __init__(self, cutoff: int = 40):
-        self.cutoff = cutoff
+    def get_metric(self, reset: bool = False):
+        return 0.0
 
-    def _get_metric(self):
+    def reset(self):
         pass
