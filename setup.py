2b868fe1f4bf7bb4d782e347518704f8c1edaca1
Joe Barrow <joseph.d.barrow@gmail.com>
2019-01-09 20:57:28 -0500

    Corrected setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='NeuCLIR',
     version='0.1dev',
-    packages=['neuclir','neuclir.readers','neuclir.models','neuclir.datasets','neuclir.metrics'],
+    packages=['neuclir','neuclir.readers','neuclir.models','neuclir.datasets','neuclir.metrics', 'neuclir.predictors'],
     license='Academic License',
     long_description=open('README.md').read(),
 )
