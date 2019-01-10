f5dfb6d3bcbda1fa55e95a6beeacb9ec0d832083
Joe Barrow <joseph.d.barrow@gmail.com>
2019-01-09 20:40:42 -0500

    Updated to take in weight parameter

--- neuclir/scripts.py
+++ neuclir/scripts.py
@@ -5,17 +5,18 @@ import logging
 from .datasets import OnlineDatasetGenerator
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         logging.log(logging.ERROR, 'Script called incorrectly. Cannot continue.')
 
     config_file = sys.argv[1]
+    weights = sys.argv[2]
     with open(config_file) as fp:
         config = json.load(fp)
 
     # load dataset generator file
     dataset = OnlineDatasetGenerator(config['dataset']['reranking'], config['systems']).sample_dataset()
     # second, run the model and get the output
-    archive = load_archive('/home/joe/projects/model.tar.gz',
+    archive = load_archive(weights,
         cuda_device=-1,
         overrides='{model: {predicting: true}, dataset_reader: { type: "reranking_dataset_reader" }}')
     predictor = Predictor.from_archive(archive, 'online_predictor')
